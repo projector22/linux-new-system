2bdb90123325e1fcb3a8fd7cf4bedb1009915cc5
projector22 <39297549+projector22@users.noreply.github.com>
2019-05-02 23:40:37 +0200

    Create webserver-centos.sh

---
+++ webserver-centos.sh
@@ -0,0 +1,16 @@
+!#/bin/bash
+echo "Installing Apache"
+sudo yum clean all
+sudo yum -y install httpd
+sudo nano /etc/httpd/conf/httpd.conf
+sudo firewall-cmd --permanent --add-port=80/tcp
+sudo firewall-cmd --permanent --add-port=443/tcp
+sudo firewall-cmd --reload
+sudo systemctl start httpd
+sudo systemctl enable httpd
+echo "Installing phpMyAdmin
+sudo yum install epel-release
+sudo yum install phpmyadmin
+echo "Replace the 127.0.0.1 with 192.168.1.0/24
+sudo nano /etc/httpd/conf.d/phpMyAdmin.conf
+sudo systemctl restart httpd
