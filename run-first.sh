3aebcb0cc9643f5646d758f3792c7aab12df282e
Gareth Palmer <39297549+projector22@users.noreply.github.com>
2019-10-21 21:46:59 +0200

    Update run-first.sh

--- run-first.sh
+++ run-first.sh
@@ -2,7 +2,6 @@
 
 #This script is meant to run before anything else, once the repo is cloned to install all the bits and pieces I've made for myself
 
-cd
-sudo mkdir bin
-cd bin
+mkdir ~/bin
+cd ~/bin
 git clone https://github.com/projector22/linux-new-system.git .
