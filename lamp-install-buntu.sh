13d12c62f8f040c9a2e6f0c0baa596824cbad62a
Gareth Palmer <gareth@Palmer-PCL.localdomain>
2019-10-23 14:33:32 +0200

    Added phpmyadmin auto

--- lamp-install-buntu.sh
+++ lamp-install-buntu.sh
@@ -43,6 +43,15 @@ sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$username'@'localhost' IDENTIFIED
 sudo mysql -e "FLUSH PRIVILEGES;"
 
 echo "Setting up phpMyAdmin"
+# WIP
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-user string root"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password root"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password root"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password root"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-websever multiselect apache2"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/database-type select mysql"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/setup-password password root"
 
 sudo apt install phpmyadmin -y
 
