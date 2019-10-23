80798ab0ab3642d415c01a441db03413075c4159
Gareth Palmer <gareth@Palmer-PCL.localdomain>
2019-10-23 13:09:14 +0200

    Added some more first run installers

--- run-first.sh
+++ run-first.sh
@@ -2,6 +2,18 @@
 
 #This script is meant to run before anything else, once the repo is cloned to install all the bits and pieces I've made for myself
 
+sudo apt install git -yy
+sudo apt install openssh-server -yy
+sudo apt install curl -yy
+
+echo "Enter a Git global name"
+read name
+echo "Enter a Git global email account"
+read mail
+
+git config --global user.name "$name"
+git config --global user.mail "$mail"
+
 mkdir ~/bin
 cd ~/bin
 git clone https://github.com/projector22/linux-new-system.git .
