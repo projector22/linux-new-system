3e96d2105ed02ba14de926c0d4b32da2ab02603a
Gareth Palmer <bravdthepally@gmail.com>
2021-05-19 14:36:27 +0200

    Added git config

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -80,3 +80,13 @@ install_php
 echo "Step 4. Configure Apache2 and PHP"
 configure_php
 configure_apache
+
+echo "Enter a Git global name"
+read name
+echo "Enter a Git global email account"
+read mail
+
+git config --global user.name "$name"
+git config --global user.mail "$mail"
+
+echo -e "\nComplete"
