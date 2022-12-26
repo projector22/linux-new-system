6d87de3519ad75bdd26090ab44cbeca47476a52c
Gareth Palmer <bravdthepally@gmail.com>
2022-12-26 21:04:44 +0200

    Added init & clear #7

--- src/linux-new-system.sh
+++ src/linux-new-system.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-# echo "cheese"
+export INSTALL_DIR=~/bin
+export APPS_DIR=$INSTALL_DIR/apps
+export SRC_DIR=$INSTALL_DIR/src
 
-cd ~/bin/apps
-git clone https://github.com/projector22/up.git
-ln -s ~/bin/apps/up/up.sh ~/bin/up
+function initialize_app() {
+    cd $SRC_DIR
+    sh clone-projects.sh
+    return 0
+}
+
+function clear_apps() {
+    cd $SRC_DIR
+    sh delete-projects.sh
+    return 0
+}
+
+case $1 in
+    --init)
+    initialize_app
+    ;;
+
+    --clear)
+    clear_apps
+    ;;
+
+    *)
+    echo "Please use one parse one of the appropriate arguments."
+    ;;
+esac
