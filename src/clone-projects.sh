00e0ab9a7c6514b2947f2e1f849bcc0451bc25c4
Gareth Palmer <bravdthepally@gmail.com>
2022-12-26 21:30:27 +0200

    Improved user feedback #7

--- src/clone-projects.sh
+++ src/clone-projects.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo -n "Setting up UP..."
 cd $APPS_DIR
-git clone https://github.com/projector22/up.git
-ln -s $APPS_DIR/up/up.sh $INSTALL_DIR/up
+git clone --quiet https://github.com/projector22/up.git 2>&0
+ln -s $APPS_DIR/up/up.sh $INSTALL_DIR/up 2>&0
+echo " Done"
--- src/delete-projects.sh
+++ src/delete-projects.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Unlink the symbolic links
 cd $INSTALL_DIR
 unlink up
+
+# Delete the cloned directories
 cd $APPS_DIR
 rm -rf up
